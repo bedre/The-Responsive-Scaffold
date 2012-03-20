c0b5bb01ed22fb5f045698d82d8e5622d3a721ba
Torkil Sinkaberg Johnsen <torkil@torkiljohnsen.com>
2012-03-20 15:12:55 +0100

    Fixing the base HTML, moved language to settings
    Signed-off-by: Torkil Sinkaberg Johnsen <torkil@torkiljohnsen.com>

--- trunk/root/index.php
+++ trunk/root/index.php
@@ -1,75 +1,54 @@
 <?php 
+/** 
+ * The Responsive Scaffold
+ * 
+ * Based on HTML5 boilerplate, http://h5bp.com
+ *
+ * Paid for and distributed freely by the great people of Skagen Hotel, Bodø: http://skagen-hotel.no
+ * Built by Torkil Sinkaberg Johnsen, BEDRE reklame, http://bedre.no
+ *
+ * External components have their respective licences.
+ * Other than that, the licence is to be considered public domain
+ */
 
-    /** 
-     * A responsive webdesign scaffold
-     * 
-     * Based on HTML5 boilerplate, http://h5bp.com
-     *
-     * Paid for and distributed freely by the great people of Skagen Hotel, Bodø: http://skagen-hotel.no
-     * Built by Torkil Sinkaberg Johnsen, BEDRE reklame, http://bedre.no
-     *
-     */
-
-    // Some settings first
-    $lang = 'en';
+require_once 'settings.php';
 ?>
 <!doctype html>
-    <!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="<?php echo $lang ?>"> <![endif]-->
-    <!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="<?php echo $lang ?>"> <![endif]-->
-    <!--[if IE 8]>    <html class="no-js lt-ie9" lang="<?php echo $lang ?>"> <![endif]-->
-    <!--[if gt IE 8]><!--> <html class="no-js" lang="<?php echo $lang ?>"> <!--<![endif]-->
-    <head>
-        <meta charset="utf-8">
-        <title>A responsive HTML5 template</title>
-        <meta name="description" content="This is an example of a responsive HTML5 template, based on HTML5 boilerplate">
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="<?php echo $lang ?>"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="<?php echo $lang ?>"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="<?php echo $lang ?>"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="<?php echo $lang ?>"> <!--<![endif]-->
 
-  <!-- Mobile viewport optimized: h5bp.com/viewport -->
-  <meta name="viewport" content="width=device-width">
-
-  <!-- Place favicon.ico and apple-touch-icon.png in the root directory: mathiasbynens.be/notes/touch-icons -->
-
-  <link rel="stylesheet" href="css/style.css">
-
-  <!-- More ideas for your <head> here: h5bp.com/d/head-Tips -->
-
-  <!-- All JavaScript at the bottom, except this Modernizr build.
-       Modernizr enables HTML5 elements & feature detects for optimal performance.
-       Create your own custom Modernizr build: www.modernizr.com/download/ -->
-  <script src="js/libs/modernizr-2.5.3.min.js"></script>
+<head>
+    <meta charset="utf-8">
+    <title>A responsive HTML5 template</title>
+    <meta name="description" content="This is an example of a responsive HTML5 template, based on HTML5 boilerplate">
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="css/screen.css">
+    <script src="js/modernizr-2.5.3.min.js"></script>
 </head>
+
 <body>
-  <!-- Prompt IE 6 users to install Chrome Frame. Remove this if you support IE 6.
-       chromium.org/developers/how-tos/chrome-frame-getting-started -->
-  <!--[if lt IE 7]><p class=chromeframe>Your browser is <em>ancient!</em> <a href="http://browsehappy.com/">Upgrade to a different browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to experience this site.</p><![endif]-->
-  <header>
+    <header>
+        Header
+    </header>
+    
+    <div role="main">
+        Main
+    </div>
+    
+    <footer>
+        Footer
+    </footer>
 
-  </header>
-  <div role="main">
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/libs/jquery-1.7.1.min.js"><\/script>')</script>
 
-  </div>
-  <footer>
-
-  </footer>
-
-
-  <!-- JavaScript at the bottom for fast page loading -->
-
-  <!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if offline -->
-  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-  <script>window.jQuery || document.write('<script src="js/libs/jquery-1.7.1.min.js"><\/script>')</script>
-
-  <!-- scripts concatenated and minified via build script -->
-  <script src="js/plugins.js"></script>
-  <script src="js/script.js"></script>
-  <!-- end scripts -->
-
-  <!-- Asynchronous Google Analytics snippet. Change UA-XXXXX-X to be your site's ID.
-       mathiasbynens.be/notes/async-analytics-snippet -->
-  <script>
-    var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
-    (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-    g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
-    s.parentNode.insertBefore(g,s)}(document,'script'));
-  </script>
+    <script>
+        var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+        (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+        g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+        s.parentNode.insertBefore(g,s)}(document,'script'));
+    </script>
 </body>
 </html>
