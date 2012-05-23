a6214955352e764567fbc1746baf4a483aadb199
Torkil Sinkaberg Johnsen <torkil@torkiljohnsen.com>
2012-05-23 19:58:48 +0200

    Converted <?php echo to <?=
    It's the way to go according to
    https://github.com/pmjones/fig-standards/blob/psr-1-style-guide/proposed
    /PSR-1-basic.md

--- trunk/responsive_template/index.php
+++ trunk/responsive_template/index.php
@@ -15,30 +15,30 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
 require_once 'settings.php';
 ?>
 <!doctype html>
-<!--[if IE 7]>    <html class="no-js lt-ie10 lt-ie9 lt-ie8" <?php echo $settings['html_attribs'] ?>> <![endif]-->
-<!--[if IE 8]>    <html class="no-js lt-ie10 lt-ie9" <?php echo $settings['html_attribs'] ?>> <![endif]-->
-<!--[if IE 9]>    <html class="no-js lt-ie10" <?php echo $settings['html_attribs'] ?>> <![endif]-->
-<!--[if (gt IE 9)|!(IE)]><!--> <html class="no-js" <?php echo $settings['html_attribs'] ?>> <!--<![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie10 lt-ie9 lt-ie8" <?= $settings['html_attribs'] ?>> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie10 lt-ie9" <?= $settings['html_attribs'] ?>> <![endif]-->
+<!--[if IE 9]>    <html class="no-js lt-ie10" <?= $settings['html_attribs'] ?>> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--> <html class="no-js" <?= $settings['html_attribs'] ?>> <!--<![endif]-->
 
 <head>
     <jdoc:include type="head" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="<?php echo $settings['template_folder'] ?>/css/screen.css">
-    <script src="<?php echo $settings['template_folder'] ?>/js/lib/modernizr-2.5.3.min.js"></script>
+    <link rel="stylesheet" href="<?= $settings['template_folder'] ?>/css/screen.css">
+    <script src="<?= $settings['template_folder'] ?>/js/lib/modernizr-2.5.3.min.js"></script>
     <script>
         Modernizr.load({
             test: Modernizr.mediaqueries,
-            nope: '<?php echo $settings['template_folder'] ?>/js/lib/respond.min.js'
+            nope: '<?= $settings['template_folder'] ?>/js/lib/respond.min.js'
         });
     </script>
 
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="<?php echo $settings['template_folder'] ?>/js/libs/jquery-1.7.2.min.js"><\/script>')</script>
-    <script src="<?php echo $settings['template_folder'] ?>/js/lib/jquery.flexslider-min.js"></script>
-    <script src="<?php echo $settings['template_folder'] ?>/js/flexslider_mask.js"></script>
+    <script>window.jQuery || document.write('<script src="<?= $settings['template_folder'] ?>/js/libs/jquery-1.7.2.min.js"><\/script>')</script>
+    <script src="<?= $settings['template_folder'] ?>/js/lib/jquery.flexslider-min.js"></script>
+    <script src="<?= $settings['template_folder'] ?>/js/flexslider_mask.js"></script>
 
     <!--[if (gte IE 6)&(lte IE 8)]>
-        <script type="text/javascript" src="<?php echo $settings['template_folder'] ?>/js/lib/selectivizr-min-1.0.2.js"></script>
+        <script type="text/javascript" src="<?= $settings['template_folder'] ?>/js/lib/selectivizr-min-1.0.2.js"></script>
     <![endif]-->
 
     <script type="text/javascript" charset="utf-8">
@@ -91,18 +91,18 @@ require_once 'settings.php';
 <body> 
     <header>
         <div class="wrap">
-            <div id="logo"><a href="/" title="<?php echo JText::_('TPL_RWDTPL_GOTO_FRONTPAGE', true); ?>"><img src="<?php echo $settings['template_folder'] ?>/images/<?php echo $settings['template_style'] ?>/logo.png" alt="<?php echo $this->params->get('company_name');?>"></a></div>
+            <div id="logo"><a href="/" title="<?= JText::_('TPL_RWDTPL_GOTO_FRONTPAGE', true); ?>"><img src="<?= $settings['template_folder'] ?>/images/<?= $settings['template_style'] ?>/logo.png" alt="<?= $this->params->get('company_name');?>"></a></div>
 
             <div id="extra">
                 <div id="language">
                     <a id="english" href="#english">English</a>
                     <a id="norwegian" href="#norwegian" class="active">Norwegian</a>
-                    <span><?php echo JText::_('Switch to') ?>: </span>
+                    <span><?= JText::_('Switch to') ?>: </span>
                 </div>
 
                 <div id="contact_short">
-                    <a href="mailto:<?php echo $this->params->get('email');?>" class="email"><span class="email_icon"> </span><?php echo $this->params->get('email');?></a>
-                    <span class="phone"><span class="phone_icon"> </span><?php echo $this->params->get('phone');?></span>
+                    <a href="mailto:<?= $this->params->get('email');?>" class="email"><span class="email_icon"> </span><?= $this->params->get('email');?></a>
+                    <span class="phone"><span class="phone_icon"> </span><?= $this->params->get('phone');?></span>
                 </div>
             </div>
 
@@ -119,7 +119,7 @@ require_once 'settings.php';
                 <li> 
                     <a href="#">
                         <div class="imagemask">
-                            <img src="<?php echo $settings['template_folder'] ?>/images/<?php echo $settings['template_style'] ?>/slideshow_image.png" alt="Slide 1">
+                            <img src="<?= $settings['template_folder'] ?>/images/<?= $settings['template_style'] ?>/slideshow_image.png" alt="Slide 1">
                         </div>
                         <div class="wrap">
                             <div class="infobox">
@@ -133,7 +133,7 @@ require_once 'settings.php';
                 <li> 
                     <a href="#">
                         <div class="imagemask">
-                            <img src="<?php echo $settings['template_folder'] ?>/images/<?php echo $settings['template_style'] ?>/slideshow_image.png" alt="Slide 2">
+                            <img src="<?= $settings['template_folder'] ?>/images/<?= $settings['template_style'] ?>/slideshow_image.png" alt="Slide 2">
                         </div>
                         <div class="wrap">
                             <div class="infobox">
@@ -147,7 +147,7 @@ require_once 'settings.php';
                 <li> 
                     <a href="#">
                         <div class="imagemask">
-                            <img src="<?php echo $settings['template_folder'] ?>/images/<?php echo $settings['template_style'] ?>/slideshow_image.png" alt="Slide 3">
+                            <img src="<?= $settings['template_folder'] ?>/images/<?= $settings['template_style'] ?>/slideshow_image.png" alt="Slide 3">
                         </div>
                         <div class="wrap">
                             <div class="infobox">
@@ -161,7 +161,7 @@ require_once 'settings.php';
                 <li> 
                     <a href="#">
                         <div class="imagemask">
-                            <img src="<?php echo $settings['template_folder'] ?>/images/<?php echo $settings['template_style'] ?>/slideshow_image.png" alt="Slide 4">
+                            <img src="<?= $settings['template_folder'] ?>/images/<?= $settings['template_style'] ?>/slideshow_image.png" alt="Slide 4">
                         </div>
                         <div class="wrap">
                             <div class="infobox">
@@ -181,7 +181,7 @@ require_once 'settings.php';
             <article class="product_category">
                 <a href="#">
                     <span class="imageholder">
-                        <img src="<?php echo $settings['template_folder'] ?>/images/<?php echo $settings['template_style'] ?>/product_image.png" alt="Product 1">
+                        <img src="<?= $settings['template_folder'] ?>/images/<?= $settings['template_style'] ?>/product_image.png" alt="Product 1">
                         <h3>Fantastic product 1</h3>
                     </span>
                     <p class="description">An awesome product description for this product</p>
@@ -190,7 +190,7 @@ require_once 'settings.php';
             <article class="product_category">
                 <a href="#">
                     <span class="imageholder">
-                        <img src="<?php echo $settings['template_folder'] ?>/images/<?php echo $settings['template_style'] ?>/product_image.png" alt="Product 2">
+                        <img src="<?= $settings['template_folder'] ?>/images/<?= $settings['template_style'] ?>/product_image.png" alt="Product 2">
                         <h3>Fantastic product 2</h3>
                     </span>
                     <p class="description">An awesome product description for this product</p>
@@ -199,7 +199,7 @@ require_once 'settings.php';
             <article class="product_category">
                 <a href="#">
                     <span class="imageholder">
-                        <img src="<?php echo $settings['template_folder'] ?>/images/<?php echo $settings['template_style'] ?>/product_image.png" alt="Product 3">
+                        <img src="<?= $settings['template_folder'] ?>/images/<?= $settings['template_style'] ?>/product_image.png" alt="Product 3">
                         <h3>Fantastic product 3</h3>
                     </span>
                     <p class="description">An awesome product description for this product</p>
@@ -208,7 +208,7 @@ require_once 'settings.php';
             <article class="product_category">
                 <a href="#">
                     <span class="imageholder">
-                        <img src="<?php echo $settings['template_folder'] ?>/images/<?php echo $settings['template_style'] ?>/product_image.png" alt="Product 4">
+                        <img src="<?= $settings['template_folder'] ?>/images/<?= $settings['template_style'] ?>/product_image.png" alt="Product 4">
                         <h3>Fantastic product 4</h3>
                     </span>
                     <p class="description">An awesome product description for this product</p>
@@ -226,22 +226,22 @@ require_once 'settings.php';
 
             <div itemscope itemtype="http://schema.org/Organization" id="contact">
                 <a name="contact"></a>
-                <h3><?php echo JText::_('TPL_RWDTPL_CONTACT_US'); ?></h3>
+                <h3><?= JText::_('TPL_RWDTPL_CONTACT_US'); ?></h3>
                 <div id="company_contact">
-                    <span itemprop="name"><?php echo $this->params->get('company_name');?></span>
-                    <span itemprop="telephone"><span id="phone-icon" class="icon"> </span><?php echo $this->params->get('phone');?></span>
-                    <a itemprop="url" href="<?php echo $this->params->get('facebook_url');?>" target="_blank"><span id="facebook-icon" class="icon"> </span><?php echo $this->params->get('facebook_url');?></a>
+                    <span itemprop="name"><?= $this->params->get('company_name');?></span>
+                    <span itemprop="telephone"><span id="phone-icon" class="icon"> </span><?= $this->params->get('phone');?></span>
+                    <a itemprop="url" href="<?= $this->params->get('facebook_url');?>" target="_blank"><span id="facebook-icon" class="icon"> </span><?= $this->params->get('facebook_url');?></a>
                 </div>
 
                 <div itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
-                    <span itemprop="description"><?php echo $this->params->get('company_name');?></span>
-                    <span itemprop="streetAddress"><?php echo $this->params->get('street_address');?></span>
-                    <span itemprop="postalCode"><?php echo $this->params->get('postal_code');?></span>
-                    <span itemprop="addressLocality"><?php echo $this->params->get('locality');?></span>
-                    <span itemprop="addressRegion"><?php echo $this->params->get('region');?></span>
-                    <span itemprop="addressCountry"><?php echo $this->params->get('country');?></span>
-                    <a itemprop="url" href="<?php echo $this->params->get('map_url');?>"><?php echo JText::_('TPL_RWDTPL_MAP'); ?></a>
-                    <img id="map" src="<?php echo $settings['template_folder'] ?>/images/<?php echo $settings['template_style'] ?>/norway_map.png" alt="Map of Norway">
+                    <span itemprop="description"><?= $this->params->get('company_name');?></span>
+                    <span itemprop="streetAddress"><?= $this->params->get('street_address');?></span>
+                    <span itemprop="postalCode"><?= $this->params->get('postal_code');?></span>
+                    <span itemprop="addressLocality"><?= $this->params->get('locality');?></span>
+                    <span itemprop="addressRegion"><?= $this->params->get('region');?></span>
+                    <span itemprop="addressCountry"><?= $this->params->get('country');?></span>
+                    <a itemprop="url" href="<?= $this->params->get('map_url');?>"><?= JText::_('TPL_RWDTPL_MAP'); ?></a>
+                    <img id="map" src="<?= $settings['template_folder'] ?>/images/<?= $settings['template_style'] ?>/norway_map.png" alt="Map of Norway">
                 </div>
             </div>
 
@@ -250,7 +250,7 @@ require_once 'settings.php';
     </footer>
 
     <script>
-        var _gaq=[['_setAccount','<?php echo $settings['analytics_acct'] ?>'],['_trackPageview']];
+        var _gaq=[['_setAccount','<?= $settings['analytics_acct'] ?>'],['_trackPageview']];
         (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
         g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
         s.parentNode.insertBefore(g,s)}(document,'script'));
