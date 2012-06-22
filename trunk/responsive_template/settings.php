e486d3f585b81377c67e90956967f9b6facffcbd
Torkil Sinkaberg Johnsen <torkil@torkiljohnsen.com>
2012-06-22 15:41:59 +0200

    Analytics account

--- trunk/responsive_template/settings.php
+++ trunk/responsive_template/settings.php
@@ -1,6 +1,6 @@
 <?php
     $settings = array(
-        'analytics_acct'  => 'UA-XXXXX-X',
+        'analytics_acct'  => 'UA-22071559-1',
         'html_attribs'    => 'xmlns="http://www.w3.org/1999/xhtml" xml:lang="'.$this->language.'" lang="'.$this->language.'" dir="'.$this->direction.'"',
         'template_folder' => $this->baseurl .'/templates/'. $this->template,
         'template_style'  => 'skagen'
